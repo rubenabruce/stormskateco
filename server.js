6b628c62d9e5e34f1870d92c341dfb9964efc5e3
Ruben Aaronovitch-Bruce <rabruben@hotmail.co.uk>
2022-01-11 10:41:58 +0000

    Service account detilas more secure

--- server.js
+++ server.js
@@ -17,7 +17,19 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const PORT = process.env.PORT || 5000;
-const serviceAccount = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const serviceAccount = {
+	type: process.env.type,
+	project_id: process.env.project_id,
+	private_key_id: process.env.private_key_id,
+	private_key: process.env.private_key,
+	client_email: process.env.client_email,
+	client_id: process.env.client_id,
+	auth_uri: process.env.auth_uri,
+	token_uri: process.env.token_uri,
+	auth_provider_x509_cert_url: process.env.auth_provider_x509_cert_url,
+	client_x509_cert_url: process.env.client_x509_cert_url,
+};
+console.log(serviceAccount);
 
 const app = express();
 
